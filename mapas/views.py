85bf424e738d3eaed09b9a3d37081f2f4729aa28
Adriano-FER <adrianofp-94@hotmail.es>
2023-09-19 19:04:53 -0300

    rotando a formato básico

--- mapas/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def mapas(request):
-    template = loader.get_template('mapas.html')
-    return HttpResponse(template.render())
