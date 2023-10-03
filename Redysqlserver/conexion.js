f1975c36e86711fe238bbb931ec9b668b5afe6a2
IvanHalen96 <ivanhalen96@gmail.com>
2023-10-02 21:17:34 -0300

    Red
    Subida de la parte de red y conexion del sql server

---
+++ Redysqlserver/conexion.js
@@ -0,0 +1,56 @@
+/*const { Connection } = require("tedious")
+
+const config ={
+    server:'DESKTOP-Q6DB84O\SQLEXPRESS',
+    authentication:{
+        type:'default',
+        options:{
+            username:"Jorge",
+            password:"123456789"
+        }
+    },
+    options:{
+        database:'VIVIVERDE',
+        port: 1433,
+        trustServerCertificate: true
+    }
+}
+const connection = new Connection(config);
+
+connection.connect();
+connection.on('connect',(err)=>{
+    if(err){
+        console.log("Error al conectarse a la base de datos");
+        console.log(err);
+    }
+    else{
+        executeStatement();
+    }
+});
+
+function executeStatement(){
+    console.log("Conectado a la base de datos");
+}*/
+var Connection = require('tedious').Connection;  
+var config = {  
+    server: 'DESKTOP-Q6DB84O\SQLEXPRESS',  //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'Jorge', //update me
+            password: '123456789'  //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'VIVIVERDE'  //update me
+    }
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.
+    console.log("Connected");  
+});
+
+connection.connect();
